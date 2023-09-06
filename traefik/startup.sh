f5daa166ef3201be4995f2eba7c23195c31856ed
IsaacDSC <isaac8.silva@hotmail.com>
2023-09-06 09:51:56 -0300

    feat: primary commit

---
+++ traefik/startup.sh
@@ -0,0 +1,2 @@
+docker-compose up -d
+docker-compose up --scale whoami=3 -d
---
+++ traefik/test.sh
@@ -0,0 +1,9 @@
+http GET http://localhost:80/ accept="whoami.docker.localhost"
+
+
+http GET whoami.docker.localhost
+
+curl whoami.docker.localhost
+
+curl -H Host:fastapi.localhost http://127.0.0.1
+curl -H Host:fastapi.localhost http://localhost
